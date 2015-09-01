2aadee717ac68e3dc7dd032593ee371f3bdb158f
KN Shrinidhi <karanam.shrinidhi@gmail.com>
2015-09-01 12:59:30 +0530

    Test CI with service hook

--- DemoGit/src/com/demogit/kns/Demo.java
+++ DemoGit/src/com/demogit/kns/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Demo Git");
+		System.out.println("CI test for service hook");
 	}
 
 }
